c0a3f90a1e2304929f210789d1bce578641df4d7
hasindu-nagolla <116471812+hasindu-nagolla@users.noreply.github.com>
2025-10-22 07:57:37 +0530

    fix triggers

--- HasiiMusic/utils/thumbnails.py
+++ HasiiMusic/utils/thumbnails.py
@@ -5,7 +5,7 @@ import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from py_yt  import VideosSearch
 from config import YOUTUBE_IMG_URL
-from Tune.core.dir import CACHE_DIR 
+from HasiiMusic.core.dir import CACHE_DIR 
 
 
 PANEL_W, PANEL_H = 763, 545
@@ -91,8 +91,8 @@ async def get_thumb(videoid: str) -> str:
     # Draw details
     draw = ImageDraw.Draw(bg)
     try:
-        title_font = ImageFont.truetype("Tune/assets/thumb/font2.ttf", 32)
-        regular_font = ImageFont.truetype("Tune/assets/thumb/font.ttf", 18)
+        title_font = ImageFont.truetype("HasiiMusic/assets/thumb/font2.ttf", 32)
+        regular_font = ImageFont.truetype("HasiiMusic/assets/thumb/font.ttf", 18)
     except OSError:
         title_font = regular_font = ImageFont.load_default()
 
@@ -114,7 +114,7 @@ async def get_thumb(videoid: str) -> str:
     draw.text((BAR_X + BAR_TOTAL_LEN - (90 if is_live else 60), BAR_Y + 15), end_text, fill="red" if is_live else "black", font=regular_font)
 
     # Icons
-    icons_path = "Tune/assets/thumb/play_icons.png"
+    icons_path = "HasiiMusic/assets/thumb/play_icons.png"
     if os.path.isfile(icons_path):
         ic = Image.open(icons_path).resize((ICONS_W, ICONS_H)).convert("RGBA")
         r, g, b, a = ic.split()
