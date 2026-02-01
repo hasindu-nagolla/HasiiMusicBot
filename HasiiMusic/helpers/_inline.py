4e7bfd383e5f4241c2da740791c4ecc75426244d
Hasindu Nagolla <116471812+hasindu-nagolla@users.noreply.github.com>
2026-02-01 05:56:59 +0530

    Fix help button KeyError

--- HasiiMusic/helpers/_inline.py
+++ HasiiMusic/helpers/_inline.py
@@ -99,8 +99,8 @@ class Inline:
             cbs = ["admins", "auth", "blist", "games", "lang",
                    "ping", "play", "queue", "stats", "sudo"]
             buttons = [
-                self.ikb(text=_lang[f"help_{i}"], callback_data=f"help {cb}")
-                for i, cb in enumerate(cbs)
+                self.ikb(text=_lang[f"help_{cb}"], callback_data=f"help {cb}")
+                for cb in cbs
             ]
             rows = [buttons[i: i + 3] for i in range(0, len(buttons), 3)]
 
